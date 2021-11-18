d4886ec146f485bd78ca4caefbf9ca90499c3b00
ljpop <80583587+ljpop@users.noreply.github.com>
2021-11-18 16:41:21 +0100

    Working on single invoice

--- invoice-app/src/components/SingleInvoice.js
+++ invoice-app/src/components/SingleInvoice.js
@@ -3,13 +3,13 @@ import './SingleInvoice.css';
 const SingleInvoice = ({singledata}) => {
     return (
         <div className='single-invoice'>
-            <div className = 'singleItem'><span className='name-date'>#</span>{singledata.id}</div>
+            <div className = 'singleItem id'><span className='hash'>#</span>{singledata.id}</div>
            <div className = 'singleItem name-date '>Due {singledata.paymentDue} </div>
            <div className = 'singleItem name-date '>{singledata.clientName} </div>
-           <div className = 'singleItem'>{singledata.total} </div>
+           <div className = 'singleItem total'>{singledata.total} </div>
 
 {/* /*TODO: draft light */}
-           <div className = 'singleItem status' style = {{textTransform:'capitalize', color: singledata.status==='paid'? '#33D69F' : (singledata.status==='draft' ? '#373B53': '#FF8F00'), backgroundColor: singledata.status==='pending'? 'rgba(255,143,0,0.06)' : 'white'}}> <span className='little-circle'>•</span> {singledata.status}  </div>
+           <div className = 'singleItem status' style = {{textTransform:'capitalize', color: singledata.status==='paid'? '#33D69F' : (singledata.status==='draft' ? '#373B53': '#FF8F00'), backgroundColor: singledata.status==='pending'? 'rgb(255,249,240)' : 'transparent'}}> <span className='little-circle'>•</span> {singledata.status}  </div>
         </div>
     )
 }
