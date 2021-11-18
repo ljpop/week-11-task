f43d1d2dfb66b50db8232e30a7a7e50f8caf9e95
ljpop <80583587+ljpop@users.noreply.github.com>
2021-11-18 15:15:32 +0100

    fixed header work on single invoice

--- invoice-app/src/components/SingleInvoice.js
+++ invoice-app/src/components/SingleInvoice.js
@@ -3,13 +3,13 @@ import './SingleInvoice.css';
 const SingleInvoice = ({singledata}) => {
     return (
         <div className='single-invoice'>
-            <div className = 'singleItem'>{singledata.id}</div>
-           <div className = 'singleItem'>Due {singledata.paymentDue} </div>
-           <div className = 'singleItem'>{singledata.clientName} </div>
+            <div className = 'singleItem'><span className='name-date'>#</span>{singledata.id}</div>
+           <div className = 'singleItem name-date '>Due {singledata.paymentDue} </div>
+           <div className = 'singleItem name-date '>{singledata.clientName} </div>
            <div className = 'singleItem'>{singledata.total} </div>
 
 {/* /*TODO: draft light */}
-           <div className = 'singleItem' style = {{textTransform:'capitalize', color: singledata.status==='paid'? '#33D69F' : (singledata.status==='pending' ? '#FF8F00': '#373B53')}}> <span className='little-circle'>•</span> {singledata.status}  </div>
+           <div className = 'singleItem status' style = {{textTransform:'capitalize', color: singledata.status==='paid'? '#33D69F' : (singledata.status==='draft' ? '#373B53': '#FF8F00'), backgroundColor: singledata.status==='pending'? 'rgba(255,143,0,0.06)' : 'white'}}> <span className='little-circle'>•</span> {singledata.status}  </div>
         </div>
     )
 }
