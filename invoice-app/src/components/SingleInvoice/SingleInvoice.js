6a2531b671989a1edba4f0d936e07d8c6a72e2f8
ljpop <80583587+ljpop@users.noreply.github.com>
2021-11-19 16:39:46 +0100

    Added arrow

--- invoice-app/src/components/SingleInvoice/SingleInvoice.js
+++ invoice-app/src/components/SingleInvoice/SingleInvoice.js
@@ -1,4 +1,5 @@
 import './SingleInvoice.css';
+import ArrowRight from "../../assets/icon-arrow-right.svg";
 
 const SingleInvoice = ({singledata}) => {
     return (
@@ -7,10 +8,10 @@ const SingleInvoice = ({singledata}) => {
            <div className = 'singleItem name-date '>Due {singledata.paymentDue} </div>
            <div className = 'singleItem name-date '>{singledata.clientName} </div>
            <div className = 'singleItem total'>{singledata.total} </div>
-
-{/* /*TODO: draft light */}
            <div className = 'singleItem status' style = {{textTransform:'capitalize', color: singledata.status==='paid'? '#33D69F' : (singledata.status==='draft' ? '#373B53': '#FF8F00'), backgroundColor: singledata.status==='paid'? 'rgb(243, 253, 250)' :(singledata.status==='draft' ? 'rgb(244,244,245)': 'rgb(255,249,240)') }}> <span className='little-circle'>•</span> {singledata.status}  </div>
-        </div>
+           <div><img className='arrow-right'
+              src={ArrowRight} alt=''/></div>
+            </div>
     )
 }
 
