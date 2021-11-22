7acfa45d29516d7819072cefa0dc85349c675555
ljpop <80583587+ljpop@users.noreply.github.com>
2021-11-22 21:34:08 +0100

    Root back to home

--- invoice-app/src/components/Header/Header.js
+++ invoice-app/src/components/Header/Header.js
@@ -1,25 +1,29 @@
-import './Header.css';
+import "./Header.css";
 import Logo from "../../assets/logo.svg";
 import Avatar from "../../assets/image-avatar.jpg";
 import Moon from "../../assets/icon-moon.svg";
-
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className='logo-img header-item'>            <img className='logo-img-circle'
-              src={Logo} alt=''/>
-            <div className='logo-img-down'></div>
-            </div>
-            <div className='header-item'>
-            <div className='light-mode'><img className='light-img'
-              src={Moon} alt=''/></div>
-            <div className='user-img'><img className='user-img-avatar'
-              src={Avatar} alt=''/></div>
-              </div>
+  return (
+    <div className="header">
+      <div className="logo-img header-item">
+        {" "}
+        <Link to="/">
+          <img className="logo-img-circle" src={Logo} alt="" />{" "}
+        </Link>
+        <div className="logo-img-down"></div>
+      </div>
+      <div className="header-item">
+        <div className="light-mode">
+          <img className="light-img" src={Moon} alt="" />
         </div>
-    )
-}
+        <div className="user-img">
+          <img className="user-img-avatar" src={Avatar} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
